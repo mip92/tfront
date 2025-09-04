c3f6408adbf4da0280ab21ebd26742666f243bc6
Oleksandr Boichenko <oleksandr.boichenko@clockwise.software>
2025-09-04 21:19:10 +0300

    Test commit with linting errors

---
+++ test-lint.ts
@@ -0,0 +1,8 @@
+// Test file with linting errors
+const unusedVariable = 'test';
+const anotherUnused = 'another';
+
+export function testFunction() {
+  console.log('test');
+  // Missing return statement
+}
